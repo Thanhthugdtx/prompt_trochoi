817cd2177d15e29cbe1e5384600c29b5c847bd26
Thanhthugdtx <thanhthuspt@gmail.com>
2026-01-29 14:50:05 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+        base: '/T-o-promt-thi-t-k-tr-ch-i/',
       server: {
         port: 3000,
         host: '0.0.0.0',
